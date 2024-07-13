63c127bcbd9292fe8751314f9101acf87991d5c2
AxelGarciaR <20220127@ricaldone.edu.sv>
2024-07-13 17:21:46 -0600

    Select en pantalla productos funcional

--- HanamiStore/views/Productos.js
+++ HanamiStore/views/Productos.js
@@ -1,21 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, TouchableOpacity, StyleSheet, Image, Dimensions, Alert } from 'react-native';
+import { View, FlatList, TouchableOpacity, StyleSheet, Image, Dimensions, Alert , RefreshControl} from 'react-native';
 import { Card, Title, Paragraph, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import fetchData from "../utils/fechdata"; // Importación de utilidad para fetch
+import ProductoCard from '../components/ProductoCard';
 
-// Datos de ejemplo (reemplazados por la carga desde la API en getProductos)
-const productos = [
-  { id: '1', nombre: 'Esencia de planta té verde', descripcion: 'Descripción 1', precio: '$8.19', calificacion: 4.8, imagen: require('../assets/skincare.png') },
-  { id: '2', nombre: 'Esencia de planta té verde', descripcion: 'Descripción 2', precio: '$6.29', calificacion: 4.5, imagen: require('../assets/skincare.png') },
-  { id: '3', nombre: 'Esencia de planta té verde', descripcion: 'Descripción 3', precio: '$6.29', calificacion: 4.5, imagen: require('../assets/skincare.png') },
-  { id: '4', nombre: 'Esencia de planta té verde', descripcion: 'Descripción 4', precio: '$7.19', calificacion: 4.7, imagen: require('../assets/skincare.png') },
-  { id: '5', nombre: 'Esencia de planta té verde', descripcion: 'Descripción 5', precio: '$5.99', calificacion: 4.6, imagen: require('../assets/skincare.png') },
-];
-
-const Productos = () => {
-  const [dataProductos, setDataProductos] = useState([]); // Estado para almacenar productos desde la API
-  const [refreshing, setRefreshing] = useState(false); // Estado para controlar el estado de recarga
+const Productos = ({ navigation }) => {
+  const [dataProductos, setDataProductos] = useState([]); // Estado para almacenar los productos más recientes
+  const [refreshing, setRefreshing] = useState(false); // Estado para controlar el estado de refrescado de la lista
 
   // Función para obtener productos desde la API
   const getProductos = async () => {
@@ -43,38 +35,33 @@ const Productos = () => {
     }, 200); // Tiempo de espera para la recarga
   };
 
-  const navigation = useNavigation();
-  const [numColumns, setNumColumns] = useState(2); // Estado para el número de columnas en la lista
   const windowWidth = Dimensions.get('window').width; // Ancho de la pantalla actual
 
-  // Renderización de cada elemento de la lista
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => navigation.navigate('DetalleProducto', { productId: item.id })} style={styles.touchable}>
-      <Card style={styles.card}>
-        <Image source={item.imagen} style={styles.image} />
-        <Card.Content>
-          <Title style={styles.title}>{item.nombre}</Title>
-          <View style={styles.ratingContainer}>
-            <Text style={styles.rating}>{item.calificacion}</Text>
-            <Text style={styles.star}>★</Text>
-          </View>
-          <Paragraph style={styles.price}>{item.precio}</Paragraph>
-        </Card.Content>
-      </Card>
-    </TouchableOpacity>
-  );
+   // Efecto para cargar los productos más recientes al cargar el componente
+   useEffect(() => {
+    getProductos();
+  }, []);
 
   return (
     <View style={{ flex: 1, padding: 8 }}>
       <FlatList
-        data={productos} // Cambiar a dataProductos para utilizar datos cargados desde la API
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        numColumns={numColumns}
-        key={numColumns} // Forzar renderización cuando cambia el número de columnas
-        refreshing={refreshing} // Propiedad para indicar que está en proceso de recarga
-        onRefresh={onRefresh} // Función para manejar la recarga de productos
-      />
+            style={styles.flatlist}
+            data={dataProductos}
+            keyExtractor={(item) => item.id_Producto.toString()}
+            numColumns={2}
+            columnWrapperStyle={styles.flatlistColumnWrapper}
+            renderItem={({ item }) => (
+              <ProductoCard
+                Nombre_Producto={item.Nombre_Producto}
+                precio_producto={item.precio_producto}
+                navigation={navigation}
+                idProducto={item.id_Producto}
+              />
+            )}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} /> // Componente de control de refresco
+            }
+          />
     </View>
   );
 };
